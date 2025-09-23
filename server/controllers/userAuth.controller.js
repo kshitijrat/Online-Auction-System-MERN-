dafabef33ce4eee9fccd12b0b1af46a2cd55a7f4
kshitijrat <kshitijratnawat@gmail.com>
2025-09-23 12:44:15 +0530

    update

--- server/controllers/userAuth.controller.js
+++ server/controllers/userAuth.controller.js
@@ -31,8 +31,8 @@ export const handleUserLogin = async (req, res) => {
         // Set HTTP-only cookie
         res.cookie("auth_token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
